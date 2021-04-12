6f0d88c53694a3132ae117bc64b28c2b9774f476
bzp2010 <bzp2010@apache.org>
2021-04-12 10:15:36 +0800

    feat: support new URP

--- htmlprovider.js
+++ htmlprovider.js
@@ -1,15 +1,15 @@
 function scheduleHtmlProvider(iframeContent = "", frameContent = "", dom = document) {
-    const frs = dom.getElementsByTagName("frame");
+	// 发起课表请求
+	let courseXHR = new XMLHttpRequest();
+	courseXHR.withCredentials = true;
+	courseXHR.open('GET', 'http://stu.j.tjcu.edu.cn/student/courseSelect/thisSemesterCurriculum/ajaxStudentSchedule/curr/callback', false);
+	courseXHR.send();
 
-    for (let i = 0; i < frs.length; i++) {
-        const dom = frs[i].contentDocument.body;
+	// 发起时间表请求
+	let timeXHR = new XMLHttpRequest();
+	timeXHR.withCredentials = true;
+	timeXHR.open('GET', 'http://stu.j.tjcu.edu.cn/ajax/getSectionAndTime?ff=f', false);
+	timeXHR.send();
 
-        if (frs[i].id === 'mainF') {
-            return dom.innerHTML;
-        } else {
-            if (result = scheduleHtmlProvider(iframeContent, frameContent, dom)) {
-                return result;
-            }
-        }
-    }
+	return JSON.stringify([JSON.parse(courseXHR.responseText), JSON.parse(timeXHR.responseText)]);
 }
--- parser.js
+++ parser.js
@@ -1,151 +1,61 @@
-//课程时间段
-let sectionTimes = [
-    {
-        "section": 1,
-        "startTime": "08:00",
-        "endTime": "08:45"
-    },
-    {
-        "section": 2,
-        "startTime": "08:50",
-        "endTime": "09:35"
-    },
-    {
-        "section": 3,
-        "startTime": "10:05",
-        "endTime": "10:50"
-    },
-    {
-        "section": 4,
-        "startTime": "10:55",
-        "endTime": "11:40"
-    },
-    {
-        "section": 5,
-        "startTime": "13:30",
-        "endTime": "14:15"
-    },
-    {
-        "section": 6,
-        "startTime": "14:20",
-        "endTime": "15:05"
-    },
-    {
-        "section": 7,
-        "startTime": "15:35",
-        "endTime": "16:20"
-    },
-    {
-        "section": 8,
-        "startTime": "16:25",
-        "endTime": "17:10"
-    },
-    {
-        "section": 9,
-        "startTime": "18:00",
-        "endTime": "18:45"
-    },
-    {
-        "section": 10,
-        "startTime": "18:50",
-        "endTime": "19:35"
-    },
-    {
-        "section": 11,
-        "startTime": "19:40",
-        "endTime": "20:25"
-    },
-    {
-        "section": 12,
-        "startTime": "20:30",
-        "endTime": "21:15"
-    },
-];
-function scheduleHtmlParser(html) {
-    //解析课程HTML
-    let table = $('table.displayTag')[1].children[3].children;
-    table = table.filter((item) => {
-        return 'name' in item && item.name === 'tr';
-    });
-    
-	//课程数据
-    let courses = table.map((item, index) => {
-        let dataColumn = filterTd(item.children);
+function scheduleHtmlParser(text) {
+	const json = JSON.parse(text);
 
-        //是否为课表主条目
-        let isMainItem = dataColumn.length === 18;
+	// 生成时间表
+	let timeData = json[1];
+	let timeResult = [];
+	timeData.sectionTime.map((item, index) = > {
+		timeResult.push({
+			"section": index + 1,
+			"startTime": item.startTime.substr(0, 2) + ":" + item.startTime.substr(2, 2),
+			"endTime": item.endTime.substr(0, 2) + ":" + item.endTime.substr(2, 2)
+		});
+	});
 
-        //处理课程周次
-        let weekList = removeNbsp(dataColumn[isMainItem ? 11 : 0].children[0].data).trim();
-        if (weekList) {
-            if (weekList.indexOf("-") !== -1) {
-                let weekListSE = weekList.replace('周上', '').split("-").map((item) => {
-                    return parseInt(item);
-                });
+	// 生成课程表
+	let courseData = json[0]['xkxx'][0];
+	let courseDataValue = Object.values(courseData);
+	let courseResult = [];
 
-                weekList = [];
-                for (let i = 0; i <= weekListSE[1] - weekListSE[0]; i++) {
-                    weekList.push(weekListSE[0] + i);
-                }
-            } else if (weekList.indexOf(",") !== -1) {
-                weekList = weekList.replace('周上', '').split(",").map((item) => {
-                    return parseInt(item);
-                });
-            } else {
-                weekList = [parseInt(weekList.replace('周上', ''))]
-            }
-        }
+	let test = [];
 
-        //生成课程数据
-        //为分支条目获取其对应的主条目
-        let lastMainItem = getLastMainItem(table, index);
+	courseDataValue.map((item, index) = > {
+		if (item.timeAndPlaceList.length === 0) { // 没有线下课
+			return;
+		}
 
-        //课程起止时间参数
-        let section = parseInt(removeNbsp(dataColumn[isMainItem ? 13 : 2].children[0].data).trim());
-        let sectionLong = parseInt(removeNbsp(dataColumn[isMainItem ? 14 : 3].children[0].data).trim());
-        let sectionArray = [];
+		// 每一种不同的课程上法都要生成一个课程记录项
+		item.timeAndPlaceList.map((tapItem) = > {
+			let weekList = tapItem.classWeek.split('');
+			let sectionList = [];
 
-        //课程时段数组生成
-        for(let i = 0; i < sectionLong; i++){
-            sectionArray.push({
-                section: section + i
-            });
-        }
+			// 生成每课程的节次列表
+			for (let i = 0; i < tapItem.continuingSession; i++) {
+				sectionList.push({
+					section: tapItem.classSessions + i
+				});
+			}
 
-        //课程数据
-        let data = {
-            name: removeNbsp((isMainItem ? dataColumn[2] : lastMainItem[2]).children[0].data).trim(),
-            position: removeNbsp(dataColumn[isMainItem ? 17 : 6].children[0].data).trim(),
-            teacher: removeNbsp((isMainItem ? dataColumn[7] : lastMainItem[7]).children[0].data).replace('*', '').trim(),
-            weeks: weekList,
-            day: parseInt(removeNbsp(dataColumn[isMainItem ? 12 : 1].children[0].data).trim()),
-            sections: sectionArray
-        };
+			let data = {
+				name: item.courseName,
+				position: tapItem.teachingBuildingName + " " + tapItem.classroomName,
+				teacher: item.attendClassTeacher,
+				weeks: weekList.map((weekItem, index) = > {
+					return weekItem === "1" ? index + 1 : false;
+				}).filter((weekItem) = > {
+					return !!weekItem
+				}),
+				day: tapItem.classDay + '',
+				sections: sectionList
+			};
 
-        return data;
-    });
+			// 生成课程数据
+			courseResult.push(data);
+		});
+	});
 
-    return { courseInfos: courses, sectionTimes }
-}
-
-//移除&nbsp;
-function removeNbsp(str) {
-    return str.replace('&nbsp;', '');
-}
-
-//过滤table-td
-function filterTd(arr) {
-    return arr.filter((item) => {
-        return 'name' in item && item.name === 'td';
-    });
-}
-
-//获取上一个主课程条目(上一个children为18个的课程条目)
-function getLastMainItem(arr, currentIndex) {
-    let tempArr = arr.slice(0, currentIndex).reverse();
-    for (let i = 0; i < tempArr.length; i++) {
-        let tempItem = filterTd(tempArr[i].children);
-        if (tempItem.length === 18) return tempItem;
-    }
-    return false;
+	return {
+		courseInfos: courseResult,
+		sectionTimes: timeResult
+	}
 }
